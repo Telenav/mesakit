e2a468f07f4854541f5e714f26eab133bb986d37
jonathan <jon@thanlocke.com>
2021-09-03 21:20:26 -0700

    👷 Add build feature branch workflow

---
+++ .github/scripts/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#
+#  © 2011-2021 Telenav, Inc.
+#  Licensed under Apache License, Version 2.0
+#
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#
+# Environment
+#
+
+ROOT="$(pwd)"
+BRANCH="${GITHUB_REF//refs\/heads\//}"
+SUPERPOM_BUILD="mvn --batch-mode --no-transfer-progress clean install"
+BUILD="mvn -Dmaven.javadoc.skip=true -DKIVAKIT_DEBUG="!Debug" -P shade -P tools --no-transfer-progress --batch-mode clean install"
+CLONE="git clone --branch "$BRANCH" --quiet"
+
+#
+# Build kivakit
+#
+
+echo "Cloning kivakit"
+cd "$ROOT"
+$CLONE https://github.com/Telenav/kivakit.git
+
+echo "Installing kivakit super POM"
+cd "$ROOT"/kivakit/superpom
+$SUPERPOM_BUILD
+
+echo "Building kivakit"
+cd "$ROOT"/kivakit
+$BUILD
+
+#
+# Build kivakit-extensions
+#
+
+echo "Cloning kivakit-extensions"
+cd "$ROOT"
+$CLONE https://github.com/Telenav/kivakit-extensions.git
+
+echo "Building kivakit-extensions"
+cd "$ROOT"/kivakit-extensions
+$BUILD
+
+#
+# Build mesakit
+#
+
+echo "Cloning mesakit"
+cd "$ROOT"
+$CLONE https://github.com/Telenav/mesakit.git
+
+echo "Installing mesakit super POM"
+cd "$ROOT"/mesakit/superpom
+$SUPERPOM_BUILD
+
+echo "Building mesakit"
+cd "$ROOT"/mesakit
+$BUILD
+
