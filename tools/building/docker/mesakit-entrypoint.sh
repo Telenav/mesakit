01ff5f5caa6c911706558047845f92dea26fd451
Jonathan Locke <jonathanl@telenav.com>
2022-01-26 07:51:19 -0700

    👷 fix mesakit entrypoint

--- tools/building/docker/mesakit-entrypoint.sh
+++ tools/building/docker/mesakit-entrypoint.sh
@@ -16,9 +16,19 @@ echo "┋  mesakit-docker-workspace.sh [host|container]  switch between host and
 echo "┋  mesakit-feature-start.sh [branch]             start a feature branch *"
 echo "┋  mesakit-feature-finish.sh [branch]            finish a feature branch *"
 echo "┋"
+echo "┋  kivakit-[tab]                                 see kivakit shell scripts"
+echo "┋  kivakit-version.sh                            show kivakit version"
+echo "┋  kivakit-build.sh                              build kivakit"
+echo "┋  kivakit-git-pull.sh                           pull changes **"
+echo "┋  kivakit-git-checkout.sh [branch]              check out the given branch **"
+echo "┋  kivakit-docker-workspace.sh [host|container]  switch between host and container workspaces"
+echo "┋  kivakit-feature-start.sh [branch]             start a feature branch **"
+echo "┋  kivakit-feature-finish.sh [branch]            finish a feature branch **"
+echo "┋"
 echo "┋ * executes the command in each mesakit repository"
+echo "┋ ** executes the command in each kivakit repository"
 echo "┋"
 echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
 echo " "
 
-/root/kivakit-entrypoint.sh
+$SHELL
