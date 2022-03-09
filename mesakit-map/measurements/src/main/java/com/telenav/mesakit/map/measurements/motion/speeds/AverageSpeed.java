509b4ae12a8273997b8bcd38d3a759874bda6218
jonathan <jon@thanlocke.com>
2022-03-09 16:11:32 -0700

    🚧 Work on updating microservice project

--- mesakit-map/measurements/src/main/java/com/telenav/mesakit/map/measurements/motion/speeds/AverageSpeed.java
+++ mesakit-map/measurements/src/main/java/com/telenav/mesakit/map/measurements/motion/speeds/AverageSpeed.java
@@ -18,7 +18,7 @@
 
 package com.telenav.mesakit.map.measurements.motion.speeds;
 
-import com.telenav.kivakit.math.statistics.Average;
+import com.telenav.kivakit.core.math.Average;
 import com.telenav.lexakai.annotations.LexakaiJavadoc;
 import com.telenav.lexakai.annotations.UmlClassDiagram;
 import com.telenav.lexakai.annotations.associations.UmlRelation;
