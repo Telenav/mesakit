a8ecdb1a06f7fcbf49afb50edeeba4764cf98b3b
jonathan <jon@thanlocke.com>
2022-03-16 11:09:56 -0600

    🔖 Update to 1.4.1-SNAPSHOT for kivakit and 0.9.11-SNAPSHOT for mesakit

--- tools/building/mesakit-build.sh
+++ tools/building/mesakit-build.sh
@@ -11,7 +11,7 @@ source mesakit-library-functions.sh
 source mesakit-library-build.sh
 source mesakit-projects.sh
 
-cd "$MESAKIT_HOME"/superpom
+cd "$MESAKIT_HOME"/superpom || exit
 mvn --batch-mode --no-transfer-progress clean install
 
 mvn --batch-mode --no-transfer-progress install:install-file -Dfile="$MESAKIT_HOME/mesakit-map/geography/libraries/shapefilereader-1.0.jar" -DgroupId=org.nocrala -DartifactId=shapefilereader -Dversion=1.0 -Dpackaging=jar
--- tools/releasing/mesakit-snapshot-update-version.sh
+++ tools/releasing/mesakit-snapshot-update-version.sh
@@ -16,11 +16,11 @@ export help="[version]"
 
 require_variable version "[version]"
 
-export snapshot_version="${1%-SNAPSHOT}-SNAPSHOT"
+export snapshot_version="${version%-SNAPSHOT}-SNAPSHOT"
 
 for project_home in "${MESAKIT_REPOSITORY_HOMES[@]}"; do
 
-    update_version "$project_home" "$version"
+    update_version "$project_home" "$snapshot_version"
 
 done
 
