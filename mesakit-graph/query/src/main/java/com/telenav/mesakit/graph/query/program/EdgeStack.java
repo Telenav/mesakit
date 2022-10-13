389990e632f22972acdb28c783133a575aab47a3
jonathanl-telenav <jonathanl@telenav.com>
2022-10-13 14:49:00 -0600

    mapped -> map
    Affected Checkouts
    ------------------
    
      * kivakit
      * lexakai
      * mesakit
      * (root)
    
    ##### Provenance
    
    Generated by cactus: *cactus-git* version _1.5.31_.
    
      * Mojo:		CommitMojo
      * Generation-Time:	2022-10-13T20:49:00Z
      * Plugin-Build:	yellow rabbit
      * Plugin-Date:	2022.09.10
      * Plugin-Commit:	f32552996bf3511e8901928b6467df125bdebbd2
      * Plugin-Repo-Clean:	true

--- mesakit-graph/query/src/main/java/com/telenav/mesakit/graph/query/program/EdgeStack.java
+++ mesakit-graph/query/src/main/java/com/telenav/mesakit/graph/query/program/EdgeStack.java
@@ -144,7 +144,7 @@ public class EdgeStack
         {
             return "[empty]";
         }
-        return ObjectList.list(stack).reversed().mapped(edge -> edge.asString(Format.PROGRAMMATIC)).join(":") + " [top]";
+        return ObjectList.list(stack).reversed().map(edge -> edge.asString(Format.PROGRAMMATIC)).join(":") + " [top]";
     }
 
     /**
