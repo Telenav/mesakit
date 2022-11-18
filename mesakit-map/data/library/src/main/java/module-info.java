c63af7d64cf436136404d2911bd552f36a6e2014
jonathanl-telenav <jonathanl@telenav.com>
2022-11-17 17:08:56 -0700

    🚧 work in progress on demodularizing kivakit

--- mesakit-map/data/library/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module mesakit.map.data.library
-{
-    requires transitive kivakit.resource;
-
-    exports com.telenav.mesakit.map.data.formats.library;
-    exports com.telenav.mesakit.map.data.formats.library.map.identifiers;
-}
--- mesakit-map/ui/debug/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-open module mesakit.map.ui.debug
-{
-    requires transitive mesakit.map.geography;
-    requires transitive mesakit.map.ui.desktop;
-
-    exports com.telenav.mesakit.map.ui.debug.debuggers;
-    exports com.telenav.mesakit.map.ui.debug.theme;
-    exports com.telenav.mesakit.map.ui.debug;
-}
