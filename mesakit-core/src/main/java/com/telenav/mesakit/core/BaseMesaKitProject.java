334c2871c74955fafefc2aa7b7d7fd754b385d57
jonathanl-telenav <jonathanl@telenav.com>
2022-12-08 14:07:20 -0700

    ✨ Added project and build properties per application/project

---
+++ mesakit-core/src/main/java/com/telenav/mesakit/core/BaseMesaKitProject.java
@@ -0,0 +1,18 @@
+package com.telenav.mesakit.core;
+
+import com.telenav.kivakit.core.project.Project;
+
+/**
+ * Base class for MesaKit projects
+ *
+ * @author jonathanl (shibo)
+ */
+@SuppressWarnings("unused")
+public abstract class BaseMesaKitProject extends Project
+{
+    @Override
+    protected final Class<?> metadataType()
+    {
+        return MesaKit.class;
+    }
+}
